c4e735eec990010a2ce2c093f6647514554abdc8
Manuel Mato <mnl.mato@gmail.com>
2017-08-09 00:29:35 +0200

    Comit inicial

---
+++ app/src/main/java/com/example/manuel/swipetabs/TabAFragment.java
@@ -0,0 +1,26 @@
+package com.example.manuel.swipetabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabAFragment extends Fragment {
+
+    public TabAFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_a, container, false);
+    }
+}
